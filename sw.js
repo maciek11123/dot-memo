8b6838b3bcb4204498c34325e7f35e6dac9a4082
maciek11123 <maciek11123@users.noreply.github.com>
2026-07-17 10:10:10 -0400

    Change default theme background from yellow to white and update service worker caching to network-first

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
-const CACHE = 'dot-memo-v2';
+const CACHE = 'dot-memo-v5';
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
-    './',
-    './index.html',
-    './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
-  ])).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ])).then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(
@@ -17,6 +21,16 @@ self.addEventListener('activate', e => {
     )).then(() => self.clients.claim())
   );
 });
+
+// Network-First Strategy to allow instant live updates
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then(response => {
+      if (response.status === 200) {
+        const responseClone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, responseClone));
+      }
+      return response;
+    }).catch(() => caches.match(e.request))
+  );
 });
